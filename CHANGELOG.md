baf0b5bbd15cc28987237909a16b53ffa3f16bbc
Elco Brouwer von Gonzenbach <elco.brouwer@gmail.com>
2018-03-26 19:22:29 +0700

    Upgrade to Laravel 5.6 & Vuetify 1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## 0.1.0 - 2017-10-17
 
 - Initial release
+
+## 1.0.0 - 2018-03-27
+
+- Laravel 5.6 + Vuetify 1.0
--- resources/assets/js/pages/settings/index.vue
+++ resources/assets/js/pages/settings/index.vue
@@ -6,28 +6,27 @@
         <v-card-title primary-title class="grey lighten-4">
           <h3 class="headline mb-0">{{ $t('settings') }}</h3>
         </v-card-title>
-        <v-tabs icons centered fixed>
-          <v-tabs-bar class="grey lighten-4">
-            <v-tabs-slider color="primary"></v-tabs-slider>
-            <v-tabs-item href="#tab-person">
-              <v-icon>person</v-icon>
-              {{ $t('profile') }}
-            </v-tabs-item>
-            <v-tabs-item href="#tab-password">
-              <v-icon>lock</v-icon>
-              {{ $t('password') }}
-            </v-tabs-item>
-          </v-tabs-bar>
-          <v-divider></v-divider>
+        <v-divider></v-divider>
 
-          <v-tabs-items>
-            <v-tabs-content id="tab-person">
-              <profile-view v-on:busy="busy = $event"></profile-view>
-            </v-tabs-content>
-            <v-tabs-content id="tab-password">
-              <password-view v-on:busy="busy = $event"></password-view>
-            </v-tabs-content>
-          </v-tabs-items>
+        <v-tabs icons-and-text centered fixed>
+          <v-tabs-slider color="primary"></v-tabs-slider>
+
+          <v-tab href="#tab-person">
+            {{ $t('profile') }}
+            <v-icon>person</v-icon>
+          </v-tab>
+          <v-tab href="#tab-password">
+            {{ $t('password') }}
+            <v-icon>lock</v-icon>
+          </v-tab>
+
+          <v-tab-item id="tab-person">
+            <profile-view v-on:busy="busy = $event"></profile-view>
+          </v-tab-item>
+          <v-tab-item id="tab-password">
+            <password-view v-on:busy="busy = $event"></password-view>
+          </v-tab-item>
+
         </v-tabs>
       </v-card>
     </v-flex>
